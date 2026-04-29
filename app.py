74422cae90994b1150067024ab0682e8fa74bb90
Dinutsa <dkaptar@gmail.com>
2026-04-29 16:57:05 +0300

    Revert "Update app.py"
    This reverts commit b322480929c8bb8fa022bcabad27274000ea7943.

--- app.py
+++ app.py
@@ -267,8 +267,8 @@ if st.session_state.processed and st.session_state.sliced is not None:
 elif not st.session_state.ld:
     st.info("👈 Завантажте файл у меню зліва.")
 
-st.markdown("<br><br>", unsafe_allow_html=True) 
-st.markdown("---") 
+st.markdown("<br><br>", unsafe_allow_html=True) # Відступ
+st.markdown("---") # Лінія
 
 footer_html = """
 <div style='text-align: center; color: #6c757d; font-size: 14px;'>
