adcbd8f680110b701e252a889b32bf62ad48cb90
Dinutsa <dkaptar@gmail.com>
2026-04-29 16:41:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import streamlit.components.v1 as components
+
+def inject_gtm():
+    # Тільки ОДИН раз на самому початку додатка
+    gtm_id = "GTM-TMF4SD39"  # Ваш ID
+    gtm_js = f"""
+        <script>
+            (function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{'gtm.start':
+            new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            }})(window,document,'script','dataLayer','{gtm_id}');
+        </script>
+    """
+    # Ми додаємо невеликий height, щоб браузер точно почав це рендерити
+    components.html(gtm_js, height=1)
+
+# Викликати одразу після set_page_config
+inject_gtm()
+
 # Імпорти
 from data_loader import load_excels, get_row_bounds, slice_range
 from classification import classify_questions, QuestionType
